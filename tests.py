ef6dd03f587e57398e4b87ed9ce2666afde21e2d
ahew <Deniskazberuk@hotmail.com>
2024-05-04 18:25:32 +0200

    5 tests added

--- tests.py
+++ tests.py
@@ -22,6 +22,13 @@ class TestMushroomDataLoading(unittest.TestCase):
         self.assertEqual(m3.get_attribute('cap-shape'), 'Bell')
         self.assertEqual(m3.get_attribute('odor'), 'Anise')
 
+    def test_len_mushrooms(self): # Vérifier que tous les champignons sont bien chargés
+        self.assertEqual(len(self.mushrooms), 8124, "Il devrait y avoir 8124 champignons dans le dataset.")  
+
+    def test_len_attributes_mush(self): # Vérifier que tous les attributs sont bien chargés, et que edible est pas pris en compte 
+        self.assertEqual(len(self.mushrooms[5]._Mushroom__cara), 22, "Il devrait y avoir 22 attributs pour chaque champignon.")
+        self.assertEqual(len(self.mushrooms[12]._Mushroom__cara), 22, "Il devrait y avoir 22 attributs pour chaque champignon.")  
+
 def make_mushroom(attributes):
     ret = Mushroom(None)
     for k, v in attributes.items():
@@ -45,6 +52,42 @@ class TestBuildTree(unittest.TestCase):
         root = self.test_tree_root
         self.assertTrue(is_edible(root, make_mushroom({'odor': 'Almond'})))
         self.assertFalse(is_edible(root, make_mushroom({'odor': 'None', 'spore-print-color': 'Green'})))
+    
+    def test_tree_depth(self): 
+        def depth(node):
+            if node.is_leaf():
+                return 1
+            for edge in node.edges_:
+                return 1 + depth(edge.child_)
+        self.assertLessEqual(depth(self.test_tree_root), 5, "La profondeur de l'arbre doit être inférieure ou égale à 5")
+
+    def test_tree_nodes(self): # Vérifier la bonne division des noeuds
+            acceptable = ['odor', 'spore-print-color', 'gill-size', 'habitat', 'cap-color']
+            def check_node(node):
+                if node.is_leaf():
+                    return
+                self.assertIn(node.criterion_, acceptable, f"Le critère de division {node.criterion_} ne doit pas être utilisé")
+                for edge in node.edges_:
+                    check_node(edge.child_)
+
+class Testbuildrule(unittest.TestCase):
+    def setUp(self):
+        self.test_tree_root = build_decision_tree(load_dataset('mushrooms.csv'))
+        self.test_rule = decision_to_rule(tree_to_rule_list(self.test_tree_root))
+    
+    def test_rule_parenthesis(self):
+        parenthesis = {'}': '{', ']': '[', ')': '('}
+        stack = []
+        for c in self.test_rule:
+            if c in parenthesis.values():
+                stack.append(c)
+            elif parenthesis.get(c) is not None:
+                self.assertEqual(stack.pop(-1), parenthesis[c], "Les parenthèses ne sont pas bien équilibrées")
+        
+        self.assertEqual(len(stack), 0, "Les parenthèses ne sont pas bien équilibrées")
+
+
+
 
 
 if __name__ == '__main__':
